64b534b2887ac9d2972a716192ffa664de2150fe
Mateus Mello <mt.gatti@hotmail.com>
2020-06-29 16:15:00 -0300

    Documentado

--- Produto.cs
+++ Produto.cs
@@ -12,28 +12,38 @@ namespace Aula27_28_29_30
         private const string PATH = "Database/Produto.csv";
 
         public Produto(){
-
+            //Cria o diretório caso ele não exista
             string pasta = PATH.Split('/')[0];
             if(!Directory.Exists(pasta)){
                 Directory.CreateDirectory(pasta);
             }
             
-            using (var output = File.Create(PATH)) {
+            //Cria o arquivo caso ele não exista
+            if(!File.Exists(PATH)){
+                File.Create(PATH).Close();
             }
 
         }
 
         
-
+        
+    
         public void Cadastrar(Produto p){
-
+            /// <summary>
+            /// Método
+            /// </summary>
+            /// <returns>Acrescenta linhas em um arquivo</returns>
+            
             var linha = new string[] { p.PrepararLinha(p) };
             File.AppendAllLines(PATH, linha);
 
         }
 
         private string PrepararLinha(Produto produto){
-
+            /// <summary>
+            /// Método
+            /// </summary>
+            /// <value>Escreve no arquivo</value>
             return $"codigo={produto.Codigo};nome={produto.Nome};preco={produto.Preco}";
 
         }
